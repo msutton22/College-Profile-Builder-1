8cde27d1aa3f78d98d67264b8ed0b1d9f567de30
Student <student@msutton.local>
2016-01-20 11:59:11 -0600

    Finished setting up table view

--- College Profile Builder/ViewController.swift
+++ College Profile Builder/ViewController.swift
@@ -12,11 +12,21 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 {
 
     @IBOutlet weak var tableView: UITableView!
-    var colleges = ["University of Southern California", "University of Washington", "New York University"] 
+    var colleges = ["University of Southern California", "University of Washington", "New York University"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colleges.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath)
+        cell.textLabel?.text = colleges[indexPath.row]
+        return cell
     }
 }
 
