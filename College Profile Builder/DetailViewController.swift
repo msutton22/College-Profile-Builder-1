33117063772a0672b7a2d7700bede9c0c97e580c
Student <student@msutton.local>
2016-01-22 12:02:28 -0600

    Created Detail view controller

---
+++ College Profile Builder/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  College Profile Builder
+//
+//  Created by Maddy Sutton on 1/21/16.
+//  Copyright © 2016 Maddy Sutton. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var numOfStuTextField: UITextField!
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var collegeTextField: UITextField!
+    var college : College!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collegeTextField.text = college.name
+        locationTextField.text = college.location
+        numOfStuTextField.text = String(college.numberOfStudents)
+        imageView.image = college.image
+    }
+    @IBAction func onTappedSavedButton(sender: AnyObject) {
+        college.name = collegeTextField.text!
+        college.location = collegeTextField.text!
+        college.numberOfStudents = Int(numOfStuTextField.text!)!
+    }
+}
