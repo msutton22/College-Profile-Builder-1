4d51e04fd2aad0f246e035095daaecf6e1e0ca3d
Student <student@msutton.local>
2016-01-26 11:57:54 -0600

    Fixed Location Text Field

--- College Profile Builder/ViewController.swift
+++ College Profile Builder/ViewController.swift
@@ -28,33 +28,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         tableView.reloadData()
     }
 
-    
-    @IBAction func onEditTapped(sender: UIBarButtonItem) {
-        if sender.tag == 0 {
-          tableView.editing = true
-            sender.tag = 1
-        }
-        else {
-            tableView.editing = false
-            sender.tag == 0
-        }
-    }
-    
-    @IBAction func onTappedPlusButton(sender: UIBarButtonItem) {
-        let alert = UIAlertController(title: "Add College", message: nil, preferredStyle: .Alert)
-        alert.addTextFieldWithConfigurationHandler{(textField) -> Void in textField.placeholder = "Add College Here"
-        }
-        let cancel = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
-        alert.addAction(cancel)
-        let addAction = UIAlertAction(title: "Add", style: .Default) { (action) -> Void in
-            let collegeTextField = alert.textFields![0] as UITextField
-            self.colleges.append(College(name: collegeTextField.text!))
-            self.tableView.reloadData()
-        }
-        alert.addAction(addAction)
-        self.presentViewController(alert, animated: true, completion: nil)
-    }
-    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return colleges.count
     }
@@ -85,6 +58,32 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let dvc = segue.destinationViewController as! DetailViewController
         let index = tableView.indexPathForSelectedRow?.row
         dvc.college = colleges[index!]
+    }
+    
+    @IBAction func onEditTapped(sender: UIBarButtonItem) {
+        if sender.tag == 0 {
+            tableView.editing = true
+            sender.tag = 1
+        }
+        else {
+            tableView.editing = false
+            sender.tag = 0
+        }
+    }
+    
+    @IBAction func onTappedPlusButton(sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add College", message: nil, preferredStyle: .Alert)
+        alert.addTextFieldWithConfigurationHandler{(textField) -> Void in textField.placeholder = "Add College Here"
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        alert.addAction(cancel)
+        let addAction = UIAlertAction(title: "Add", style: .Default) { (action) -> Void in
+            let collegeTextField = alert.textFields![0] as UITextField
+            self.colleges.append(College(name: collegeTextField.text!))
+            self.tableView.reloadData()
+        }
+        alert.addAction(addAction)
+        self.presentViewController(alert, animated: true, completion: nil)
     }
 }
 
